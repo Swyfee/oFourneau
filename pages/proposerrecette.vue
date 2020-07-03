0ccc2b8b121548033fe41c8e130b381a30b7d6f1
GRazafy <gabrielrazafy@gmail.com>
2020-07-03 14:46:44 +0200

    proposerrecette

---
+++ pages/proposerrecette.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container_image">
+        <v-img
+           height="600"
+           :src="require('~/components/recette1.PNG')"
+        />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default'
+}
+</script>
